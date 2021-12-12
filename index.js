515245702195e02f86f34597d72019a2bf454fb7
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-12-12 05:33:06 +0300

    *

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 require("./db");
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport')(passport);
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(express.urlencoded({ extended: false }))
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 const roleRouter = require('./routers/routes/role');
 const userRouter = require("./routers/routes/user");
@@ -17,8 +41,7 @@ app.use(userRouter);
 app.use(postRouter);
 app.use(commentRouter);
 
-const PORT = process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 4600;
 app.listen(PORT, () => {
   console.log(`server running at port ${PORT}`);
 });
